fffe470f27f13a883b793f615050328a182b182f
Pritom Mazumder <31571051+pritom169@users.noreply.github.com>
2022-12-15 02:53:59 +0600

    largest_number updated.

--- week02/largest_number.py
+++ week02/largest_number.py
@@ -1,15 +1,26 @@
-class Solution:
-    #TC: O(nlogn) because we are utilizing the sort in python which has nlogn complexity
-    #SC: O(n) because we are we are storing the copied version of nums.
-    def largestNumber(nums: [int]) -> str:
-        for i, n in enumerate(nums):
-            nums[i] = str(n)
+from functools import cmp_to_key
 
+class Solution:
+    # TC: O(nlogn) because there is a built in sort we are using which has O(nlong) complexity.
+    # SC: O(n) because we are storing the integer numbers into a string array.
+
+    def largestNumber(self, nums: List[int]) -> str:
+        #We are creating a string array with the same integers
+        string_numbers = ['a'] * len(nums)
+        for i, n in enumerate(nums):
+            string_numbers[i] = str(nums[i])
+        
+        # A compare function compares the best possible combination for two integers.
+        # For example: In array [30, 9], 930 will be greater than 309
         def compare(n1, n2):
-            if n1 + n2 > n2 + n1:
+            if n1 + n2 > n2+ n1:
                 return -1
             else:
                 return 1
+        
+        # Sorting the array according to the compare function
+        string_numbers = sorted(string_numbers, key = cmp_to_key(compare))
 
-        nums = sorted(nums, key = cmp_to_key(compare))
-        return str(int("".join(nums)))
+        # We are appending all the elements from the sorted array and returning the number
+        # in integer format.
+        return str(int("".join(string_numbers)))
